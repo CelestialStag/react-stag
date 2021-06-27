b929e4f218ee0740eb83e9fbd54f1a058e3ecbd1
Elias Mawa <elias@emawa.io>
2021-06-26 18:14:37 -0600

    cleanup code & fix esm and cjs building

--- src/components/typography/Heading.tsx
+++ src/components/typography/Heading.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { AriaCommon } from '../../lib';
 import { BoxProps } from '../base/Box';
@@ -7,8 +7,10 @@ export type HeadingProps = {
 	size: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 } & BoxProps;
 
-export const Heading = React.forwardRef((props: HeadingProps, ref: React.Ref<HTMLHeadingElement>) => {
+const Heading = React.forwardRef((props: HeadingProps, ref: React.Ref<HTMLHeadingElement>) => {
 
+	const [ focus ] = useState(false);
+	
 	const style: React.CSSProperties = {
 		...props.style
 	};
@@ -20,10 +22,12 @@ export const Heading = React.forwardRef((props: HeadingProps, ref: React.Ref<HTM
 	return (
 		<props.size ref={ref} {...aria}
 			className={props.class ? props.class : undefined}
-			style={style}>
+			style={style} hidden={focus}>
 			{props.children}
 		</props.size>
 	);
 });
 
 Heading.displayName = 'Heading';
+
+export { Heading };
